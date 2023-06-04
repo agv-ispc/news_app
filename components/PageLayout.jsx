344dd0e7b3d95b0c31a244f548eb18712441801f
AGV Repo <agv.ispc@gmail.com>
2023-06-04 14:37:12 -0300

    230604 actualizacion

---
+++ components/PageLayout.jsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function PageLayout({ children, title='NewsApp' }) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content="Newsapp - the best app to read news" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <div>
+          <Link href='/'>🗞️ NewsApi</Link></div>
+        <div>
+          <Link href='/about'>About</Link></div>
+      </header>
+      <main>
+        {children}
+      </main>
+      <style jsx>{` 
+      header {
+        display: flex;
+        justify-content: space-between;
+        padding: 20px;
+      }
+    `}</style>
+    </>
+  )
+}
---
+++ pages/about.js
@@ -0,0 +1,6 @@
+import PageLayout from "../components/PageLayout"
+export default function About (){
+    return <PageLayout title = 'About'>
+        <h1>Esta es la pagina About</h1>
+        </PageLayout>
+}
